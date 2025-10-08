70c59160a096ebb32fbfe67af87d1f7501bd35cd
Kimjiyeon970608 <ne2210ne@naver.com>
2025-10-08 17:31:42 +0900

    naver_login folder 병합

--- src/components/NaverLogin.jsx
+++ src/components/NaverLogin.jsx
@@ -1,27 +1,40 @@
-import { useEffect } from 'react';
+import { useRef } from "react";
 
 const NaverLogin = () => {
+  const naverRef = useRef();
 
+  const handleNaverLogin = () => {
+    if (window.naver) {
+      try {
+        const clientId = import.meta.env.VITE_NAVER_CLIENT_ID;
+        const redirectUri = encodeURIComponent(
+          `${window.location.origin}/auth/naver/callback`
+        );
+        const state = Math.random().toString(36).substring(2, 15);
 
-  useEffect(() => {
-    if (window.naver_id_login) {
-      // Client ID
-      const naverLogin = new window.naver_id_login("TadebOjQaTTUUg3kWJSr", window.location.origin + "/auth/naver/callback");
-      const state = naverLogin.getUniqState();
-      naverLogin.setButton("white", 2, 40);
-      naverLogin.setDomain(window.location.origin);
-      naverLogin.setState(state);
-      naverLogin.setPopup();
-      naverLogin.init_naver_id_login();
+        const naverLoginUrl = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}`;
+
+        window.location.href = naverLoginUrl;
+      } catch (error) {
+        console.error("네이버 로그인 오류:", error);
+      }
     }
-  }, []);
-
-
-
+  };
 
   return (
-    <div id="naver_id_login"></div>
-  );
-}
+    <div>
+      {/* 네이버 SDK가 필요로 하는 숨겨진 요소 */}
+      <div ref={naverRef} id="naverIdLogin" style={{ display: "none" }}></div>
 
-export default NaverLogin
+      {/* 커스텀 로그인 버튼 */}
+      <img
+        src="/naver_login.svg"
+        alt="네이버 로그인"
+        onClick={handleNaverLogin}
+        className="w-full cursor-pointer hover:opacity-80 transition-opacity duration-300"
+      />
+    </div>
+  );
+};
+
+export default NaverLogin;
